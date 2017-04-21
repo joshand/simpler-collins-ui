430b1046fd4e60472e633dc5ea133d8ebec4a120
kecorbin <kecorbin@cisco.com>
2017-04-21 17:09:40 -0500

    small tweaks from boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Banner from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Banner />,
+  document.getElementById('root')
+);
