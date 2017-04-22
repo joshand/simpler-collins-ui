f900dec04537324d09919c534353f345e4b15f68
kecorbin <kecorbin@cisco.com>
2017-04-22 00:43:18 -0500

    add sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+var SideBar = React.createClass({
+
+  getInitialState: function() {
+    return {
+      // initialize with a blank list
+      data: []
+    }
+  },
+
+  render: function() {
+    return (
+        <div className="sidebar" data-color="blue" data-image="assets/img/sidebar.jpg">
+          <div className="sidebar-wrapper">
+            <div className="logo">
+              <a href="https://github.com/imapex/collins" className="simple-text">
+              Collins
+              </a>
+            </div>
+            <ul className="nav">
+              <li className="active">
+                <a href="/jobs"><i className="pe-7s-graph"></i><p>Jobs</p></a>
+              </li>
+              <li className="inactive">
+                <a href="/results"><i className="pe-7s-graph"></i><p>Results</p></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+    )
+  }
+});
+
+export default SideBar
